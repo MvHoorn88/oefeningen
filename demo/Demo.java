82bfa6c9ee6f628cbff7cd64d55c5e380f04ff6c
MvHoorn88 <m.hoorn88@gmail.com>
2018-05-22 12:00:12 +0200

    aantekeningen felix

---
+++ demo/Demo.java
@@ -0,0 +1,66 @@
+package demo;
+
+public class Demo {
+	public static void main(String[] args) {
+//	for(int i =1; i < 101; i++) {
+	//System.out.println(i);		}		
+	for(int x = 0 ; x < 101 ; x++){
+		System.out.println(x++);
+		
+	}
+		String [] cat = { "Lynx", "Puma", "Black Panther", "Leopard", "Cheetah", "Lion", "Panther"};					    
+		System.out.println(cat[0]);
+							
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*	int x = 16;
+		for(int i = 0; i < x ; i++){ 
+			System.out.println(i+1);			
+			}
+int[]Kaarten= {3, 12, 2, 14, 14, 13, 8, 5, 11, 4, 7, 12, 2, 5, 9};
+
+System.out.println(Kaarten);
+	}
+	
+		int hand[] = {12, 5, 9, 2, 13, 11, 5, 8 ,8 ,9, 12, 3, 4, 9, 14, 6};
+		System.out.println(hand);
+		
+	
+	public static void main(String[] args){
+		
+		StringBuilder sb = new StringBuilder("lantaarnpaallampje");	 //Constructs an object of type StringBuilder
+		sb.delete(8,12);											// int index op sb 8e till 12e character will be deleted
+		System.out.println(sb);										// index starts at 0
+		
+		
+		String lineOfTekst = "lantaarnpaallampje";
+		int a = lineOfTekst.lastIndexOf('m');
+		System.out.println(a);
+		int b = lineOfTekst.indexOf('a');
+		System.out.println(b);
+	
+		boolean startsWith = lineOfTekst.startsWith("tan");
+		System.out.println(startsWith);
+		
+		
+
+		
+		
+		String name = "Bart"; 								// replace a for e
+		System.out.println(name.replace('a','e'));			//methode werkt!
+		
+		String naam = " Bart";							    // probeert van a een e te maken
+		naam.replace('a','e');								// methode werkt niet! String is immutable!
+		System.out.println(naam);
+		*/
+	} 
+	}
+	
